ce48c5298e3d369309fb2235749633a72963d7c6
Chris Fryer <chris.fryer78@gmail.com>
2016-10-22 21:18:05 +1100

    2nd commit

--- chrisstepper.h
+++ chrisstepper.h
@@ -7,6 +7,7 @@ class ChrisStepper {
   public:
     ChrisStepper(int steppin, int dirpin, int enablepin);
     void SetSpeed(int speed);
+    void Step(int dir, int steps);
 }
 
 #endif // chrisstepper
