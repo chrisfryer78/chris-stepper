c6ae7eae3cea2e1da546ff9eb88249699f8d03fc
Chris Fryer <chris.fryer78@gmail.com>
2016-10-22 21:39:10 +1100

    3

--- chrisstepper.h
+++ chrisstepper.h
@@ -5,9 +5,11 @@
 
 class ChrisStepper {
   public:
+    int StepperNo;
     ChrisStepper(int steppin, int dirpin, int enablepin);
-    void SetSpeed(int speed);
-    void Step(int dir, int steps);
+    void SetSpeed(int StepperNo, int speed);
+    void Step(int StepperNo, int dir, int steps);
+  private:
 }
 
 #endif // chrisstepper
